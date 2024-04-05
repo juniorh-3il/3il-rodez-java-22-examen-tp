0010b8f56b06aafd5b7448652e50213c74644097
juniorh-3il <juniorh@3il.fr>
2024-04-05 14:04:06 +0200

    🚧 Création des attributs necessaires pour la classe Task

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,9 @@
+import java.util.Date;
+
 public class Task {
+    private int id;
+    private String titre;
+    private String description;
+    private Date dateEcheance;
+    private TaskPriority priorite;
 }
